d07fa183fa03c3d5fc785af67a7b9e1f14ea76fe
Sheldon H Johnson <johnsonsheldon332@gmail.com>
2021-09-02 13:30:36 -0400

    boilerplate set up

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App =()=>{
+
+    return(
+        <div>
+<h1>hi</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ client/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import {RecoilRoot} from 'recoil'
+ReactDOM.render(
+
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+      </RecoilRoot>
+  </React.StrictMode>,
+    
+      
+  document.getElementById('root')
+); 
