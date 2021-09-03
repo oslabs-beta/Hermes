f2fe2a7315c6c44bae7bfc9b8c31715f2e6a8be6
Jared Lewis <jareddlewis@gmail.com>
2021-09-02 19:28:26 -0500

    switched to webpack and resolved jsx extensions

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {RecoilRoot} from 'recoil'
+import { RecoilRoot } from 'recoil';
 ReactDOM.render(
-
   <React.StrictMode>
     <RecoilRoot>
       <App />
-      </RecoilRoot>
+    </RecoilRoot>
   </React.StrictMode>,
-    
-      
+
   document.getElementById('root')
-); 
+);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|woff|woff2|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/build/',
+    historyApiFallback: true,
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    extensions: ['.jsx', '...'],
+  },
+};
