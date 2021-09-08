f1d41ce6eb7ab2c8ecda24fb8572e3236655409b
Jared Lewis <jareddlewis@gmail.com>
2021-09-08 14:33:22 -0500

    added a line chart with sample data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,43 +1,40 @@
-import React from "react";
-import SideBar from "./SideBar";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import LogsContainer from "./LogsContainer";
-import HomePage from "./HomePage";
-import AlertsContainer from "./AlertsContainer";
-import CreateIndex from "./CreateIndex";
-import Visualizer from "./Visualizer";
-import Nav from "./Nav";
+import React from 'react';
+import SideBar from './SideBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LogsContainer from './LogsContainer';
+import HomePage from './HomePage';
+import AlertsContainer from './AlertsContainer';
+import CreateIndex from './CreateIndex';
+import Visualizer from './Visualizer';
+import Nav from './Nav';
 
-const App =()=>{
-
-  return(
+const App = () => {
+  return (
     <Router>
-     
-      <header className="header">
-        <Nav/>
+      <header className='header'>
+        <Nav />
         <h1>Hermes</h1>
       </header>
-      <div className="app-container">
-        {false && <SideBar/>}
-           
+      <div className='app-container'>
+        {false && <SideBar />}
+
         <Switch>
-          <Route exact path="/logs">
-            
-            <HomePage/>
+          <Route exact path='/'>
+            <HomePage />
           </Route>
-          <Route path="/">
-            <LogsContainer/>
-          </Route>
-
-          <Route path="/visualizer">
-            <Visualizer/>
+          <Route path='/logs'>
+            <LogsContainer />
           </Route>
 
-          <Route path="/alerts">
-            <AlertsContainer/>
+          <Route path='/visualizer'>
+            <Visualizer />
           </Route>
-          <Route path="/indexes">
-            <CreateIndex/>
+
+          <Route path='/alerts'>
+            <AlertsContainer />
+          </Route>
+          <Route path='/indexes'>
+            <CreateIndex />
           </Route>
         </Switch>
       </div>
--- client/components/Visualizer.jsx
+++ client/components/Visualizer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserLineChart from './UserLineChart';
 
 const Visualizer = () => {
   return (
     <div>
       <header className='visualizer-header'></header>
+      <UserLineChart />
     </div>
   );
 };
