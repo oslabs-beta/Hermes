71c8d153a0fc1452442218cecfa5bb9c2e9e0f2c
Jared Lewis <jareddlewis@gmail.com>
2021-09-08 18:56:17 -0500

    gets up to 10000 logs at a time

--- client/components/Visualizer.jsx
+++ client/components/Visualizer.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserLineChart from './UserLineChart';
+import axios from 'axios';
 
 const Visualizer = () => {
+  const [logs, setLogs] = useState(null);
+  useEffect(() => {
+    axios
+      .get('/logs/logsbyindex/?index=logstash-*')
+      .then((res) => {
+        setLogs(res.data.hits.hits);
+      })
+      .catch((error) => console.log('Error in Visualizer useEffect: ', error));
+  }, []);
+  // const logs = useAxios('/logs/logsbyindex/?index=logstash-*');
+  console.log(logs);
+  if (logs) {
+    const logsOnly = logs.map((hit) => {
+      return { log: hit._source.log };
+    });
+    console.log('array of logs:', logsOnly);
+    const regex = /info/i;
+    const errorLogs = logsOnly.filter((hit) => {
+      regex.test(hit.log);
+    });
+    console.log('array of logs with Errors: ', errorLogs);
+    console.log(logs[0]._source);
+  }
+
+  // const errors = logs.filter((hit) => hit.log);
+  // console.log(errors);
+
   return (
     <div>
       <header className='visualizer-header'></header>
