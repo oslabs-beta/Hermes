e38d4649506ae3639863846888e589c6f46cf937
Sheldon H Johnson <johnsonsheldon332@gmail.com>
2021-09-09 11:33:08 -0400

    Completed mvp logs display.

--- client/components/LogsContainer.jsx
+++ client/components/LogsContainer.jsx
@@ -13,19 +13,42 @@ const LogsContainer = () => {
   
   console.log(useRecoilValue(logState));
 
-  const data1 = JSON.parse(JSON.stringify(useRecoilValue(logState))) ;
-  console.log(data1);
-  const data = [
-    {id: 1, name: 'Editor', value: 5},
-    {id: 2, name: 'Grid'},
-    {id: 3, name: 'Chart'},
-    {id: 4, name: 'Cart'}
-  ];
+  const data1 = [...JSON.parse(JSON.stringify(useRecoilValue(logState)))] ;
+  if(data1[0]){
+    console.log(data1[0]['_source']);
+  }
+  const arr = [];
+  for(let i=0; i <data1.length; i++) {
+    if(data1[i]) arr.push({ ...data1[i], ...data1[i]['_source'], ...data1[i]['_source']['kubernetes']});
+  }
+
+  console.log(arr);
+  // const data = [
+  //   {id: 1, name: 'Editor', value: 5},
+  //   {id: 2, name: 'Grid'},
+  //   {id: 3, name: 'Chart'},
+  //   {id: 4, name: 'Cart'}
+  // ];
+
+  // const dataSource = {
+  //   withCredentials: false,
+  //   initialRequest: true,
+  //   api: {
+  //     readData: { url: '/logs/logsbyindex/?index=logstash-*', method: 'GET' }
+  //   }
+  // };
   
   const columns = [
     {name: '_id', header: 'ID'},
     {name: '_index', header: 'index'},
-    {name: `_source["@timestamp"]`, header: 'Source'}
+    {name: `@timestamp`, header: '@timestamp'},
+    {name: `host`, header: 'host'},
+    {name: `pod_id`, header: 'pod_id'},
+    {name: `pod_name`, header: 'pod_name'},
+    {name: `stream`, header: 'stream'},
+    {name: `container_name`, header: 'container_name'},
+    {name: `container_hash`, header: 'container_hash'}
+
   ];
   // const [logs, setLogs] = useRecoilState(logS)
 
@@ -34,17 +57,19 @@ const LogsContainer = () => {
   //     .then(res => res.json())
   //     .then(res => console.log(res));
   // }, []);
-
-
+  const [field, setField]= useState('');
+  const [value, setValue]= useState('');
   // const arr = logs.hits.hits;
   return (
     <>
       <div className="filter-box">
         <button type="button">add filter</button>
-        <select name="cars" id="cars">
+        <input type="text" className="filter-input" placeholder="field" onChange={(e) =>{setField(e.target.value);}}/>
+        <input type="text" className="filter-input" placeholder="value" onChange={(e) =>{setField(e.target.value);}}/>
+        {/* <select name="cars" id="cars">
           <option value="filters">filters</option>
           
-        </select>
+        </select> */}
       </div>
       <div className='logs'>
         <header className="logs-display-header">
@@ -54,8 +79,8 @@ const LogsContainer = () => {
 
         <div  id="logs-container">
           
-          <Grid
-            data={data1}
+          { data1[0] && <Grid
+            data={arr}
             columns={columns}
             rowHeight={25}
             bodyHeight={100}
@@ -63,7 +88,7 @@ const LogsContainer = () => {
             widthResizable={true}
             rowHeaders={['rowNum']}
           />
-
+          }
           {/* {logs.map(log => {
             return < Logs key={log._id} time={log['source'['@timestamp']]} index={log._index}  />;
           })} */}
