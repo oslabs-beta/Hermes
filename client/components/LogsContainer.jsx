7072928ca85d79ac71a1d56e87107a240a0a2da7
Jared Lewis <jareddlewis@gmail.com>
2021-09-10 10:08:14 -0500

    MVP Presentation

--- client/components/LogsContainer.jsx
+++ client/components/LogsContainer.jsx
@@ -1,26 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import Logs from "./Logs";
-import {useAxios} from "../hooks/useAxios";
-import {logState} from '../atom';
+import Logs from './Logs';
+import { useAxios } from '../hooks/useAxios';
+import { logState } from '../atom';
 import 'tui-grid/dist/tui-grid.css';
 import Grid from '@toast-ui/react-grid';
+import Row from './Row';
 
 const LogsContainer = () => {
-  const logs = useAxios('/logs/logsbyindex/?index=logstash-*');
+  const logs = useAxios('/logs/logsbyindex/?index=logstash-*&field=*&value=*');
   console.log(logs);
-  
+
   console.log(useRecoilValue(logState));
 
-  const data1 = [...JSON.parse(JSON.stringify(useRecoilValue(logState)))] ;
-  if(data1[0]){
+  const data1 = [...JSON.parse(JSON.stringify(useRecoilValue(logState)))];
+  if (data1[0]) {
     console.log(data1[0]['_source']);
   }
   const arr = [];
-  for(let i=0; i <data1.length; i++) {
-    if(data1[i]) arr.push({ ...data1[i], ...data1[i]['_source'], ...data1[i]['_source']['kubernetes']});
+  // build an array of objects. Each object has one property, Log. The value is the _source object stringified.
+  for (let i = 0; i < data1.length; i++) {
+    if (data1[i]) {
+      let string = '';
+      for (const key in data1[i]._source) {
+        string += `${key}: `;
+        if (typeof data1[i]._source[key] === 'object') {
+          string += `${JSON.stringify(data1[i]._source[key])}, `;
+        } else {
+          string += `${data1[i]._source[key]}, `;
+        }
+      }
+      arr.push(string);
+    }
   }
+  // for (let i = 0; i < data1.length; i++) {
+  //   if (data1[i])
+  //     arr.push({
+  //       ...data1[i],
+  //       ...data1[i]['_source'],
+  //       ...data1[i]['_source']['kubernetes'],
+  //     });
+  // }
 
   console.log(arr);
   // const data = [
@@ -37,19 +58,19 @@ const LogsContainer = () => {
   //     readData: { url: '/logs/logsbyindex/?index=logstash-*', method: 'GET' }
   //   }
   // };
-  
-  const columns = [
-    {name: '_id', header: 'ID'},
-    {name: '_index', header: 'index'},
-    {name: `@timestamp`, header: '@timestamp'},
-    {name: `host`, header: 'host'},
-    {name: `pod_id`, header: 'pod_id'},
-    {name: `pod_name`, header: 'pod_name'},
-    {name: `stream`, header: 'stream'},
-    {name: `container_name`, header: 'container_name'},
-    {name: `container_hash`, header: 'container_hash'}
 
-  ];
+  const columns = [{ name: 'Logs', header: 'Log' }];
+  // const columns = [
+  //   { name: '_index', header: 'index' },
+  //   { name: `@timestamp`, header: '@timestamp' },
+  //   { name: 'log', header: 'Log' },
+  //   { name: `host`, header: 'host' },
+  //   { name: `pod_id`, header: 'pod_id' },
+  //   { name: `pod_name`, header: 'pod_name' },
+  //   { name: `stream`, header: 'stream' },
+  //   { name: `container_name`, header: 'container_name' },
+  //   { name: `container_hash`, header: 'container_hash' },
+  // ];
   // const [logs, setLogs] = useRecoilState(logS)
 
   // useEffect(() =>{
@@ -57,48 +78,75 @@ const LogsContainer = () => {
   //     .then(res => res.json())
   //     .then(res => console.log(res));
   // }, []);
-  const [field, setField]= useState('');
-  const [value, setValue]= useState('');
+  const [field, setField] = useState('');
+  const [value, setValue] = useState('');
   // const arr = logs.hits.hits;
   return (
-    <>
-      <div className="filter-box">
-        <button type="button">add filter</button>
-        <input type="text" className="filter-input" placeholder="field" onChange={(e) =>{setField(e.target.value);}}/>
-        <input type="text" className="filter-input" placeholder="value" onChange={(e) =>{setField(e.target.value);}}/>
+    <div className='log-page-container'>
+      <div className='filter-box'>
+        <button type='button'>add filter</button>
+        <input
+          type='text'
+          className='filter-input'
+          placeholder='field'
+          onChange={(e) => {
+            setField(e.target.value);
+          }}
+        />
+        <input
+          type='text'
+          className='filter-input'
+          placeholder='value'
+          onChange={(e) => {
+            setField(e.target.value);
+          }}
+        />
         {/* <select name="cars" id="cars">
           <option value="filters">filters</option>
           
         </select> */}
       </div>
       <div className='logs'>
-        <header className="logs-display-header">
+        <header className='logs-display-header'></header>
 
+        <div id='logs-container'>
+          <div id='table-div'>
+            <table>
+              <tr>
+                <th className='more-info'>More Info</th>
+                <th className='more-info'>Logs</th>
+              </tr>
+              {arr.map((log, i) => (
+                <Row log={log} />
+              ))}
+            </table>
+          </div>
 
-        </header>
-
-        <div  id="logs-container">
-          
-          { data1[0] && <Grid
-            data={arr}
-            columns={columns}
-            rowHeight={25}
-            bodyHeight={100}
-            heightResizable={true}
-            widthResizable={true}
-            rowHeaders={['rowNum']}
-          />
-          }
-          {/* {logs.map(log => {
-            return < Logs key={log._id} time={log['source'['@timestamp']]} index={log._index}  />;
+          {/* {data1[0] && (
+            <Grid
+              className='log-grid'
+              data={arr}
+              columns={columns}
+              rowHeight={25}
+              bodyHeight={100}
+              heightResizable={true}
+              widthResizable={true}
+              rowHeaders={['rowNum']}
+            />
+          )} */}
+          {/* {logs.map((log) => {
+            return (
+              <Logs
+                key={log._id}
+                time={log['source'['@timestamp']]}
+                index={log._index}
+              />
+            );
           })} */}
         </div>
-
       </div>
-    </>
+    </div>
   );
 };
-
-
 
 export default LogsContainer;
