daf87efce581381f857675a54a062025968a7b3c
eric-olaya <ericolaya@gmail.com>
2021-09-16 11:07:46 -0400

    working on alerts front end

--- client/components/AlertsContainer.jsx
+++ client/components/AlertsContainer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Alerts from "./Alerts";
+import CreateAlert from './matUI/CreateAlert';
+import TextField from './matUI/AlertSearchBox';
+import Button from '@material-ui/core/Button';
 
 const AlertsContainer = () =>{
   // const logs = useAxios()
@@ -7,9 +10,19 @@ const AlertsContainer = () =>{
   return(
     <div>
       <header className="alerts-display-header">
-        <Alerts/>
+        Alerts
       </header>
+      <div className="alert-inputs">
+      <form className="alert-inputs">
+        <TextField label="test" style={{ fontSize: '16px' }}/>
+        <Button style={{ fontSize: '16px' }}>Search</Button>
+      </form>
+        <CreateAlert variant="contained" style={{ fontSize: '16px' }}></CreateAlert>
+      </div>
+      <div className='alert-box'>
+      <Alerts/>
 
+      </div>
     </div>
   );
 };
