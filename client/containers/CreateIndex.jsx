6fae9d3e4af213ddde7c684eabb05c7aed6ecbfe
Sheldon H Johnson <johnsonsheldon332@gmail.com>
2021-09-20 11:12:36 -0400

    added delete index pattern functionallity

--- client/containers/CreateIndex.jsx
+++ client/containers/CreateIndex.jsx
@@ -6,6 +6,17 @@ const CreateIndex = () =>{
 
   const [input, setInput] = useState('');
 
+  const [patterns, setPatterns] = useState([]);
+
+  const [marked, setMarked] = useState('');
+
+  useEffect(() => {
+    fetch('/indexpatterns').then(res => res.json()).then(res => setPatterns(res));
+    
+  }, [marked]);
+
+  console.log(patterns);
+
 
   useEffect(() => {
     fetch('/logs/esindices').then(res => res.json()).then(res => setAlias(res));
@@ -20,6 +31,22 @@ const CreateIndex = () =>{
 
   for(let key in alias){
     arr.push(key);
+  }
+
+  function deleter(data) {
+    fetch(`/indexpatterns`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'Application/JSON'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(resp => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+      
+      .catch(err => console.log('Login error:', err));
   }
   
   function poster(data) {
@@ -49,7 +76,13 @@ const CreateIndex = () =>{
       <header className="alerts-display-header">
         <h1 className="index-titler">Define an index pattern</h1>
         <input type="text" className="index-field" value={input} onChange={(e)=> setInput(e.target.value)}/>
-        {truer(arr, input) && <button type="button" onClick={()=> poster({'indexPattern': input})}>Add Index</button>};
+        {truer(arr, input) && <button type="button" onClick={()=> poster({'indexPattern': input})}>Add Index</button>}
+        <div className="delete-indexes">
+          <select name="patterns" className="index-patterns" onChange={(e)=> setMarked(e.target.value)}>
+            {patterns && patterns.map((index, i)=>{
+              return <option value={index} key ={i}>{index}</option>;
+            })}
+          </select><button type="button" onClick={()=> deleter({'indexPattern': marked})}>delete</button></div>
       </header>
       <div className="sources-container">
         <h1>Sources</h1>
