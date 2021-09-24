9991ecd6737a34d53f1df5ba662535b63350303e
Sheldon H Johnson <johnsonsheldon332@gmail.com>
2021-09-24 03:09:52 -0400

    finished testing for routes

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,5 +1,5 @@
 
-const { response } = require('express');
+// const { response } = require('express');
 const request = require('supertest');
 
 const server = 'http://localhost:3068';
@@ -21,6 +21,33 @@ describe('Route integration', () => {
           .get('/logs/logsbyindex')
           .expect('Content-Type', /application\/json/)
           .expect(200));
+    });
+  });
+  describe('/logs/esindices', () => {
+    describe('GET', () => {
+      it('responds with 200 status and application/json content type', () =>
+        request(server)
+          .get('/logs/esindices')
+          .expect('Content-Type', /application\/json/)
+          .expect(200));
+    });
+  });
+  describe('/logs/hourbuckets', () => {
+    describe('GET', () => {
+      it('responds with 200 status and text/html content type', () =>
+        request(server)
+          .get('/logs/hourbuckets')
+          .expect('Content-Type', /text\/html/));
+        
+    });
+  });
+  describe('/logs/monitor', () => {
+    describe('GET', () => {
+      it('responds with 200 status and text/html content type', () =>
+        request(server)
+          .get('/logs/monitor')
+          .expect('Content-Type', /text\/html/));
+ 
     });
   });
   describe('/indexpatterns', () => {
